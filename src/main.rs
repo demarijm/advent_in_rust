cfb6e8b40b8a93f4491646586185403d14b13c7c
Demari <demarijmiller@gmail.com>
2023-12-02 20:55:36 -0500

    rusty

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-use regex::Regex;
-
 mod part1;
 mod part2;
 fn main() {
-    // let filepath = "input.txt";
-    let text = "one, two, three, and seven are numbers";
-    let re = Regex::new(r"\b(one|two|three|seven)\b").unwrap();
 
-    for cap in re.find_iter(text) {
-        println!("Found: {}", &cap.as_str());
-    }
-
-
-
-
-    // part1::read_file_line_by_line(filepath).expect("Could not read file");
+    let filepath = "input.txt";
+    part1::read_file_line_by_line(filepath).expect("Could not read file");
     // part2::replace_each_line_with_numbers(filepath).expect("Could not read file");
 }
